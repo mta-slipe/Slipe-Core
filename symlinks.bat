5b22f8ba30f6541507ad13a80d2d418fe713ad10
Bob <bhooff@avans.nl>
2019-03-07 16:49:34 +0100

    Compile command fix in relation to symlinks

--- symlinks.bat
+++ symlinks.bat
@@ -1,3 +1,7 @@
+mkdir Symlinks
+mkdir Symlinks\Client
+mkdir Symlinks\Server
+
 mklink /D .\Symlinks\Server\MTASharedWrapper ..\..\Resource\MTASharedWrapper
 mklink /D .\Symlinks\Server\MTAServerWrapper ..\..\Resource\MTAServerWrapper
 mklink /D .\Symlinks\Server\MTAServerResource ..\..\Resource\MTAServerResource
