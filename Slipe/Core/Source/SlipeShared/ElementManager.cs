d017257161b32d3ab7ff573719f22669c9a8e255
Bob <bhooff@avans.nl>
2019-03-26 14:04:00 +0100

    WIP modular project setup

--- Slipe/Core/Source/SlipeShared/ElementManager.cs
+++ Slipe/Core/Source/SlipeShared/ElementManager.cs
@@ -57,6 +57,7 @@ namespace Slipe.Shared
         /// </summary>
         public void RegisterElement(Element element)
         {
+            Console.WriteLine("Registring element");
             elements.Add(element.MTAElement, element);
         }
 
--- Slipe/Lua/Main/main.lua
+++ Slipe/Lua/Main/main.lua
@@ -1,12 +1,53 @@
 require = function() end
 
+local allClasses = {}
+local mainString
 
-local filepath = triggerServerEvent == nil and "Dist/Server/manifest.lua" or "Dist/Client/manifest.lua"
-local file = fileOpen(filepath)
-local content = fileRead(file, fileGetSize(file))
-fileClose(file)
-local result = loadstring(content)
-result()()
+local oldInit = System.init
+
+local prepareInit = function(classes)
+	iprint(allClasses)
+	for _, class in ipairs(classes) do
+		allClasses[#allClasses + 1] = class
+	end
+end
+
+local finalizeInit = function(classes, settings)
+	for _, class in ipairs(classes) do
+		allClasses[#allClasses + 1] = class
+	end
+	mainString = settings.Main
+	oldInit(allClasses, settings)
+end
+
+function prepareManifest(filepath)
+	System.init = prepareInit
+
+	local file = fileOpen(filepath)
+	local content = fileRead(file, fileGetSize(file))
+	fileClose(file)
+	local result = loadstring(content)
+	result()()
+end
+
+function finalizeManifest(filepath)
+	System.init = finalizeInit
+
+	local file = fileOpen(filepath)
+	local content = fileRead(file, fileGetSize(file))
+	fileClose(file)
+	local result = loadstring(content)
+	result()()
+end
+
+function prepareModule(path)
+	local path = path .. "/Lua/Compiled/" .. ( triggerServerEvent == nil and "Server" or "Client") .. "/manifest.lua"
+	prepareManifest(path)
+end
+prepareModule("Slipe/Core")
+
+local mainManifest = triggerServerEvent == nil and "Dist/Server/manifest.lua" or "Dist/Client/manifest.lua"
+finalizeManifest(mainManifest)
 
 if triggerServerEvent == nil then
 	ServerTest.Program.Main()
