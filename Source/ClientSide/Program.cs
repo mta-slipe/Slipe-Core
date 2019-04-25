afaeb31a719b5533c2b89e5b90258d309ff5e7e0
DezZolation <mathijssonnemans@hotmail.com>
2019-04-25 14:19:32 +0200

    FaceElement method

--- Source/ClientSide/Program.cs
+++ Source/ClientSide/Program.cs
@@ -4,6 +4,7 @@ using Slipe.Client.SightLines;
 using Slipe.Client.Peds;
 using Slipe.Client.Resources;
 using System;
+using Slipe.Client.GameWorld;
 
 namespace ClientSide
 {
@@ -15,6 +16,18 @@ namespace ClientSide
         {
             new ElementManager(new ElementHelper());
             new Program();
+
+            // BROKEN UNTIL FIXED BY YANG :D
+            //try
+            //{
+            //    Player p = Player.GetFromName("SAES>DezZolation");
+            //    WorldObject ob = new WorldObject(1337, new System.Numerics.Vector3(2488, -1662, 18));
+            //    RootElement.OnRender += () =>
+            //    {
+            //        ob.FaceElement(p);
+            //    };
+            //}catch(Exception e) { }
+
         }
 
         public Program()
