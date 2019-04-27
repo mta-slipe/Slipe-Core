9a9c8bd681ad5c9d33a528413433b4707b9cc546
DezZolation <mathijssonnemans@hotmail.com>
2019-04-27 22:19:02 +0200

    Cleaned dist

--- Source/ClientSide/Program.cs
+++ Source/ClientSide/Program.cs
@@ -1,45 +1,19 @@
 ﻿using Slipe.Client.Elements;
 using Slipe.Shared.Elements;
-using Slipe.Client.SightLines;
-using Slipe.Client.Peds;
-using Slipe.Client.Resources;
-using System;
-using Slipe.Client.GameWorld;
 
 namespace ClientSide
 {
     class Program
     {
-        private Follower follower;
-
         static void Main(string[] args)
         {
             new ElementManager(new ElementHelper());
             new Program();
-
-            // BROKEN UNTIL FIXED BY YANG :D
-            //try
-            //{
-            //    Player p = Player.GetFromName("SAES>DezZolation");
-            //    WorldObject ob = new WorldObject(1337, new System.Numerics.Vector3(2488, -1662, 18));
-            //    RootElement.OnRender += () =>
-            //    {
-            //        ob.FaceElement(p);
-            //    };
-            //}catch(Exception e) { }
-
         }
 
         public Program()
         {
-            Player.Local.OnConsole += (string cmd) =>
-            {
-                if (cmd == "npc")
-                {
-                    follower = new Follower(Player.Local);
-                }
-
-            };
+           
         }
     }
 }
--- Source/ServerSide/Program.cs
+++ Source/ServerSide/Program.cs
@@ -1,13 +1,6 @@
 ﻿
 using Slipe.Server.Elements;
-using Slipe.Server.GameServer;
-using Slipe.Server.Vehicles;
 using Slipe.Shared.Elements;
-using System.Numerics;
-using Slipe.Shared.Utilities;
-using Slipe.Server.Peds;
-using Slipe.Server.Resources;
-using System;
 
 namespace ServerSide
 {
@@ -21,20 +14,7 @@ namespace ServerSide
 
         public Program()
         {           
-            Team awesome = new Team("Awesome", Color.HotPink);
-
-            foreach(Player p in Player.Alive)
-            {
-                p.Team = awesome;
-            }
-
-            TeamVehicle t = new TeamVehicle(VehicleModel.Patriot, new Vector3(1954, -2166, 15), awesome);
-
-            Gate[] gateArray = {
-                new Gate(new Vector3(1964.27f, -2189.76f, 14.37f), new Vector3(0, 0, 106.7f), new Vector3(1969.52f, -2189.78f, 14.37f)),
-                new Gate(new Vector3(1958.8f, -2189.76f, 14.37f), new Vector3(0, 0, 106.7f), new Vector3(1953.64f, -2189.77f, 14.37f))
-            };
-            GateGroup lsAirport = new GateGroup(new Vector3(1957, -2202, 12), new Vector3(1967, -2176, 17), gateArray);
+            
         }
     }
 }
