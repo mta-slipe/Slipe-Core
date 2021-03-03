c679e552bb78216dc925e6b1cb1aa3f1ae7e6581
NanoBob <info@nanobob.net>
2021-03-03 18:24:35 +0000

    Stream in/out event args typo fix

--- Slipe/Core/Source/SlipeShared/Elements/Events/OnStreamInEventArgs.cs
+++ Slipe/Core/Source/SlipeShared/Elements/Events/OnStreamInEventArgs.cs
@@ -4,8 +4,8 @@ using System.Text;
 
 namespace Slipe.Shared.Elements.Events
 {
-    public class OnSteamInEventArgs
+    public class OnStreamInEventArgs
     {
-        internal OnSteamInEventArgs() { }
+        internal OnStreamInEventArgs() { }
     }
 }
